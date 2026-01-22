1e2978b1468457cbf18c61b6986078b457b39f2d
Mike Alexander <mike@mikealex.com>
2026-01-23 00:21:37 +1100

    More case updates

--- src/core/index.js
+++ src/core/index.js
@@ -2,11 +2,11 @@
  * SVG Shape classes index
  */
 
-export { Shape } from './Shape.js';
-export { Line } from './Line.js';
-export { Circle } from './Circle.js';
-export { Rect } from './Rect.js';
-export { Arc } from './Arc.js';
-export { Pad } from './Pad.js';
-export { Via } from './Via.js';
-export { Polygon } from './Polygon.js';
+export { Shape } from './shape.js';
+export { Line } from './line.js';
+export { Circle } from './circle.js';
+export { Rect } from './rect.js';
+export { Arc } from './arc.js';
+export { Pad } from './pad.js';
+export { Via } from './via.js';
+export { Polygon } from './polygon.js';
