ff9dd6e0179046b64746e12fa450b14978a5ef69
Mike Alexander <mike@mikealex.com>
2026-09-08 02:36:11 +1000

    Clear properties and return Home after PCB deletion

--- src/pcb/modules/box-select.js
+++ src/pcb/modules/box-select.js
@@ -582,5 +582,7 @@ export function deleteBoxSelection(app) {
 
     if (cmds.length === 0) return false;
     app.history?.execute(cmds.length === 1 ? cmds[0] : new CompoundCommand(cmds));
+    app._clearProperties?.();
+    app._setActiveRibbonTab?.('pcb-home');
     return true;
 }
